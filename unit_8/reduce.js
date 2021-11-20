1bc2912deae82781c7ebfdea40f573c900a7ae33
manan <pmanandev009@gmail.com>
2021-11-19 22:53:20 -0600

    added reduce exercise

---
+++ unit_8/reduce.js
@@ -0,0 +1,96 @@
+/*
+Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+
+Examples:
+    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
+    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
+*/
+// NOTE:   
+function extractValue(arr, key) {
+    let result_arr = arr.reduce(function(acc,next_obj){
+        console.log(acc, next_obj)
+        acc.push(next_obj[key])
+        return acc;
+    },[])
+    return result_arr;
+}
+
+/*
+Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+
+Examples:
+    vowelCount('Elie') // {e:2,i:1};
+    vowelCount('Tim') // {i:1};
+    vowelCount('Matt') // {a:1})
+    vowelCount('hmmm') // {};
+    vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
+*/
+
+function vowelCount(str) {
+    //['e','l','i','e']
+    const vowels = 'aeiou'
+    let lower_str_arr = Array.from(str.toLowerCase());
+    let result = lower_str_arr.reduce(function(vowel_count_obj,next_letter){
+        console.log(vowel_count_obj, next_letter)
+        if(vowels.indexOf(next_letter) !== -1){
+            if(vowel_count_obj[next_letter]){
+            vowel_count_obj[next_letter]+=1;
+            }
+            else{
+            vowel_count_obj[next_letter] = 1;
+            }
+        }
+        
+        return vowel_count_obj;
+    },{})
+    return result;
+}
+
+/*
+Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+
+Examples:
+    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
+    
+    addKeyAndValue(arr, 'title', 'Instructor') // 
+      [
+        {title: 'Instructor', name: 'Elie'}, 
+        {title: 'Instructor', name: 'Tim'}, 
+        {title: 'Instructor', name: 'Matt'}, 
+        {title: 'Instructor', name: 'Colt'}
+       ]
+*/
+
+function addKeyAndValue(arr, key, value) {
+    let result_arr = arr.reduce(function(acc_object_array,next_obj){
+       next_obj[key] = value;
+       acc_object_array.push(next_obj);
+       return acc_object_array
+        
+    },[])
+    return result_arr;
+}
+
+/*
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+
+Examples:
+    
+    function isEven(val){
+        return val % 2 === 0;
+    }
+    
+    const arr = [1,2,3,4,5,6,7,8];
+    
+    partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
+    
+    function isLongerThanThreeCharacters(val){
+        return val.length > 3;
+    }
+    
+    const names = ['Elie', 'Colt', 'Tim', 'Matt'];
+    
+    partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
+*/
+
+function partition(arr, callback) {}
